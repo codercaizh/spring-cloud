ee34eb950ecddbbcb145c30f42d7262d9a08f892
caizhihong <545347837@qq.com>
2017-11-01 15:56:16 +0800

    SpringBoot之HelloWorld

---
+++ 1-SpringBoot/springboot-helloworld/src/main/java/com/czh/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.czh.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//RestController注解表明该控制器是一个Rest风格控制器，方法返回的对象会序列化成JSON
+@RestController
+public class HelloController {
+	
+	//GetMapping注解表明该方法只处理来自路径为"/hello"的GET请求
+	@GetMapping("/hello")
+	public Map helloWorld() {
+		Map hello = new HashMap();
+		hello.put("SpringBoot", "HelloWorld");
+		return hello;
+	}
+}
